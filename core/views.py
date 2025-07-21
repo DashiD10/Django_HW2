0271c799188198b5b65604455802d9456ec17da6
Dashi <dorzhiev_dd@mail.ru>
2025-07-21 22:54:19 +1200

    HW38. Создал функции, шаблоны, настроил пути к шаблонам

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+def landing(request):
+    return render(request, 'landing.html')
+
+def thanks(request):
+    return render(request, 'thanks.html')
+
+def orders_list(request):
+    return render(request, 'orders_list.html')
+
+def order_detail(request, order_id):
+    return render(request, 'order_detail.html')
+
+
+
 # Create your views here.
