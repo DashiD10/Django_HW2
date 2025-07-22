734bce6526b636723a16dfa10394462a85e99a76
Dashi <dorzhiev_dd@mail.ru>
2025-07-22 23:52:10 +1200

    HW38. Настроил шаблоны,подключил дата.ру, в браузере маршруты работают

--- core/views.py
+++ core/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .data import orders
 
 def landing(request):
-    return render(request, 'landing.html')
+    return render(request, 'core/landing.html')
 
 def thanks(request):
-    return render(request, 'thanks.html')
+    return render(request, 'core/thanks.html')
 
 def orders_list(request):
-    return render(request, 'orders_list.html')
+    context = {
+        "orders": orders,  # Передаем список заказов в шаблон
+    }
+    return render(request, 'core/orders_list.html', context)
 
 def order_detail(request, order_id):
-    return render(request, 'order_detail.html')
+    # Используем next() для поиска первого совпадения
+    order = next((order for order in orders if order["id"] == order_id), None)
+    
+    if order is None:
+        return HttpResponse("<h1>Заказ не найден</h1>", status=404)
+    
+    context = {
+        "order": order,
+    }
+    return render(request, "core/order_detail.html", context)
+
 
 
 
