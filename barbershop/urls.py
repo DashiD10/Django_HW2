1fa1301b209344b0abfa1e3f9e253d511bb25139
Dashi <dorzhiev_dd@mail.ru>
2025-08-07 20:14:48 +1200

    HW39 ДЗ сделал, но пока не понял как имена мастеров в карточки вложить

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from core import views
@@ -25,4 +27,4 @@ urlpatterns = [
     path('thanks/', views.thanks, name='thanks'),
     path('orders/', views.orders_list, name='orders_list'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
