50067def8b65257b8c02977b0a0241d03a8be2ee
Dashi <dorzhiev_dd@mail.ru>
2025-08-16 23:04:09 +1200

    HW42 Установил и настроил Django Debug Toolbar

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 from core import views
 
 
@@ -27,4 +28,5 @@ urlpatterns = [
     path('thanks/', views.thanks, name='thanks'),
     path('orders/', views.orders_list, name='orders_list'),
     path('orders/<int:pk>/', views.order_detail, name='order_detail'),
+    path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
